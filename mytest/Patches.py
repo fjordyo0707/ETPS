cd9b96dc976f61f64fd67bd27583543144504cf9
fjordyo0707 <33324251+fjordyo0707@users.noreply.github.com>
2019-04-22 16:28:26 +0800

    Adjust superpixel method with rough cut

--- mytest/Patches.py
+++ mytest/Patches.py
@@ -2,8 +2,10 @@ import ReadColMap
 import cv2
 import numpy as np
 import matplotlib.pyplot as plt
+import statistics
+import PIL.Image
 
-class patch:
+class Superpixel:
     def __init__(self):
         self.idx = -1
         self.rows = []
@@ -12,13 +14,33 @@ class patch:
         self.featureCols = []
         self.featureDepth = []
         self.featureNumber = 0
+        self.agentPixel = [0,0]
+        self.superDepth = -1
+        self.label = 0
 
     def Print(self):
         print('Patxh Number: ', self.idx)
         print('Number of pixel: ', len(self.rows))
         print('Number of featureRows: ', len(self.featureRows))
         print('Number of featureDepth: ', len(self.featureDepth))
+        print('agentPixel: ', self.agentPixel)
+        print('superDepth: ', self.superDepth)
+        print('Label: ', self.label)
 
+class Patch:
+    def __init__(self):
+        self.nonfeatureSuperpixel = []
+        self.featureSuperpixel = []
+    def Print(self):
+        print('Number nonfeatureSuperpixel: ', len(self.nonfeatureSuperpixel))
+        print('Number featrueSuperpixel: ', len(self.featureSuperpixel))
+
+
+def Process_Label():
+    label_png = './sample_data/frames/000026_json/label.png'
+    lbl = np.asarray(PIL.Image.open(label_png))
+    label_idx = np.unique(lbl)
+    return lbl, label_idx
 
 # I use 000029.png to demonstrate
 # The conresponding idx in Views is 28
@@ -44,11 +66,12 @@ def initialTest(input_frames, recon):
     depthSparse = recon.GetSparseDepthMap(testFrameidx)
     return depthSparse, h, w
 
-def constructPatches(inputSparseDepth):
-    print('**************start construct patches*****************')
-    patches = []
+def constructSuperpixels(inputSparseDepth, seg_lbl, seg_idx):
+    print('**************start construct Superpixels*****************')
+    superPixelArray = []
+    #label_withSuper = [[] for _ in range(len(seg_idx))]
     idxNoneZeroRowSparse, idxNoneZeroColSparse = np.nonzero(inputSparseDepth)
-    fs = cv2.FileStorage("label_500.xml", cv2.FILE_STORAGE_READ)
+    fs = cv2.FileStorage("label_1500.xml", cv2.FILE_STORAGE_READ)
     fn_label_matrix = fs.getNode("label_matrix")
     fn_label_number = fs.getNode("label_number")
     label_matrix = fn_label_matrix.mat()
@@ -56,38 +79,77 @@ def constructPatches(inputSparseDepth):
     print('label number: ', label_number)
 
     for i in range(label_number+1):
-        tempPatch = patch()
-        tempPatch.idx = i
+        tempSuper = Superpixel()
+        tempSuper.idx = i
         tempRow, tempCol = np.nonzero(label_matrix == i)
-        tempPatch.rows =  tempRow
-        tempPatch.cols = tempCol
-        #tempPatch.Print()
-        patches.append(tempPatch)
+        tempSuper.rows =  tempRow
+        tempSuper.cols = tempCol
+        tempSuper.agentPixel[0] = int(np.median(tempRow))
+        tempSuper.agentPixel[1] = int(np.median(tempCol))
+        tempSuper.label = seg_lbl[tempSuper.agentPixel[0]][tempSuper.agentPixel[1]]
+        superPixelArray.append(tempSuper)
+        #label_withSuper[int(tempSuper.label)].append(tempSuper)
 
     print('')
-    print('Number of patch: ', len(patches))
+    print('Number of Superpixel: ', len(superPixelArray))
 
     for i,j in zip(idxNoneZeroRowSparse, idxNoneZeroColSparse):
-        tempIdxPatches = label_matrix[i][j]
-        nowPatch = patches[tempIdxPatches]
-        nowPatch.featureRows.append(i)
-        nowPatch.featureCols.append(j)
-        nowPatch.featureDepth.append(inputSparseDepth[i,j])
-        #nowPatch.Print()
-    return patches
+        tempIdxSupers = label_matrix[i][j]
+        nowSuper = superPixelArray[tempIdxSupers]
+        nowSuper.featureRows.append(i)
+        nowSuper.featureCols.append(j)
+        nowSuper.featureDepth.append(inputSparseDepth[i,j])
+        nowSuper.superDepth = inputSparseDepth[i,j]
+        #nowSuper.Print()
+    return superPixelArray
 
+def constructPatches(sortSuperPixels, seg_idx):
+    label_withSuper = [[] for _ in range(len(seg_idx))]
+    patchArray = []
+    for oneSuper in sortSuperPixels:
+        label_withSuper[int(oneSuper.label)].append(oneSuper)
 
-def constructCoarseDepthMap(patches):
-    CoarseDepthMap = np.zeros((global_h,global_w))
-    for currentPatch in patches:
-
-        for row, col in zip(currentPatch.rows, currentPatch.cols):
-            if len(currentPatch.featureDepth) is not 0:
-                CoarseDepthMap[row,col] = currentPatch.featureDepth[0]
+    for onelabel in label_withSuper:
+        tempPatch = Patch()
+        for oneSuper in onelabel:
+            if oneSuper.superDepth != -1:
+                tempPatch.featureSuperpixel.append(oneSuper.idx)
             else:
-                CoarseDepthMap[row,col] = 0
-    #print(CoarseDepthMap[np.nonzero(CoarseDepthMap)])
-    return CoarseDepthMap
+                tempPatch.nonfeatureSuperpixel.append(oneSuper.idx)
+        patchArray.append(tempPatch)
+        tempPatch.Print()
+    return patchArray
+
+def propogateDepth(sortSuperPixels, patchArray):
+    DenseDepthMap = np.zeros((global_h,global_w))
+    for onePatch in patchArray:
+        ctpoint_rows = np.zeros(len(onePatch.featureSuperpixel))
+        ctpoint_cols = np.zeros(len(onePatch.featureSuperpixel))
+        ctpoint_depth = np.zeros(len(onePatch.featureSuperpixel))
+        for i in range(len(onePatch.featureSuperpixel)):
+            ctpoint_rows[i] = sortSuperPixels[onePatch.featureSuperpixel[i]].agentPixel[0]
+            ctpoint_cols[i] = sortSuperPixels[onePatch.featureSuperpixel[i]].agentPixel[1]
+            ctpoint_depth[i] = sortSuperPixels[onePatch.featureSuperpixel[i]].superDepth
+        for i in range(len(onePatch.nonfeatureSuperpixel)):
+            udpoint_row = sortSuperPixels[onePatch.nonfeatureSuperpixel[i]].agentPixel[0]
+            udpoint_col = sortSuperPixels[onePatch.nonfeatureSuperpixel[i]].agentPixel[1]
+            weighted = np.reciprocal(np.sqrt((ctpoint_rows - udpoint_row)**2 + (ctpoint_cols - udpoint_col)**2))
+            weighted_normalize = weighted/weighted.sum()
+            sortSuperPixels[onePatch.nonfeatureSuperpixel[i]].superDepth = np.multiply(weighted_normalize,ctpoint_depth).sum()
+        print('Finish one patch')
+
+    for onesuper in sortSuperPixels:
+        for row, col in zip(onesuper.rows, onesuper.cols):
+            DenseDepthMap[row][col] = onesuper.superDepth
+    return DenseDepthMap
+
+
+
+
+
+
+
+
 
 
 
@@ -110,10 +172,13 @@ def main():
 
     global_h = 0
     global_w = 0
+    seg_lbl, seg_idx = Process_Label()
     sparseDepthMap,h ,w = initialTest(input_frames, recon)
-    sortPatches = constructPatches(sparseDepthMap)
-    CoarseDepthMap = constructCoarseDepthMap(sortPatches)
-    #plt.imsave("CoarseDepthMap.png", CoarseDepthMap, cmap = 'seismic')
+    sortSuperPixels = constructSuperpixels(sparseDepthMap, seg_lbl, seg_idx)
+    patchArray = constructPatches(sortSuperPixels, seg_idx)
+    DenseDepthMap = propogateDepth(sortSuperPixels, patchArray)
+    plt.imsave("SuperDense.png", DenseDepthMap, cmap = 'viridis')
+
 
 if __name__ == '__main__':
     main()
